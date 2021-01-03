c5729d92bf63311b23989d4792acb3c90e0fabf6
sudosubin <sudosubin+github@gmail.com>
2021-01-04 00:52:21 +0900

    Add diary models

--- server/diary/models.py
+++ server/diary/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(verbose_name='일기 제목', max_length=100)
+    content = models.TextField(verbose_name='일기 내용')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.id}. {self.title}'
