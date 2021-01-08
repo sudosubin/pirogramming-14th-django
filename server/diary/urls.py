ec8935b11ab331943465a2b2f4536dab8de38dd2
sudosubin <sudosubin+github@gmail.com>
2021-01-09 01:30:03 +0900

    Redirect when no content found

--- server/diary/urls.py
+++ server/diary/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from diary.views import article_list
+from diary.views import article_list, article_read
+
+app_name = 'diary'
 
 urlpatterns = [
     path('', article_list, name='article_list'),
+    path('article/<int:pk>/', article_read, name='article_read'),
 ]
--- server/diary/views.py
+++ server/diary/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from diary.models import Article
 
@@ -7,6 +8,19 @@ from diary.models import Article
 def article_list(request: HttpRequest):
     queryset = Article.objects.all()
     context = {
-        "articles": queryset
+        'articles': queryset,
     }
-    return render(request, "diary/list.html", context)
+    return render(request, 'diary/list.html', context)
+
+
+def article_read(request: HttpRequest, pk: int):
+    try:
+        article = Article.objects.get(id=pk)
+    except Article.DoesNotExist:
+        destination = reverse('diary:article_list')
+        return redirect(destination)
+
+    context = {
+        'article': article,
+    }
+    return render(request, 'diary/read.html', context)
